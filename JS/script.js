3f707e64dee68dc24d4ae3f4e3ed14eabce33bea
Mehdi-Abshari <absharimahdi5@gmail.com>
2023-01-04 21:42:47 +0330

    Notepad local storage
    پروژه نوت پد تا قسمت ذخیره کردن متن زده شد.

--- JS/script.js
+++ JS/script.js
@@ -6,29 +6,80 @@ const noteList = document.querySelector("#note-list");
 
 // -- Events
 // submit new note
-form.addEventListener("submit", newnote);
+form.addEventListener("submit", newNote);
+document.addEventListener("DOMContentLoaded", showSavedNotes);
 
 // --Function
-function newnote(e) {
+function newNote(e) {
   e.preventDefault();
 
-  // Access to taxt area value
+  // Access to textarea value
   const noteText = document.querySelector("#note").value;
-  // Access to text input value
-  const noteTitle = document.querySelector("#title").value;
 
   // Craete <div> tag
-  let div = document.createElement("div");
-  (div.innerText = noteTitle), noteText;
+  const div = document.createElement("div");
+  div.innerText = noteText;
+  div.classList.add("noteBox");
 
-  // Craete remove button (✖)
+  // --Craete remove button (X)
+  // Create <a> tag for remove button
   const removeBtn = document.createElement("a");
+
+  // Set (X) to remove button
   removeBtn.innerText = "✖";
+  // Set class for removeBtn
   removeBtn.classList.add("removeBtn");
 
-  // Adding remove button to div
+  // Adding removeBtn to <div>
   div.appendChild(removeBtn);
-
-  // Adding <div> to the #note-list
+  // Adding <div> to noteList
   noteList.appendChild(div);
+
+  // Adding Data to localstorage
+  addToLs(noteText);
+}
+
+function addToLs(noteText) {
+  if (localStorage.getItem("historyNotes")) {
+    alert("داره!");
+    let arrayParsed = getLS();
+    arrayParsed.push(noteText);
+    let arrayStringfy = JSON.stringify(arrayParsed);
+    localStorage.setItem("historyNotes", arrayStringfy);
+  } else {
+    alert("ندارهههههه!");
+
+    let historyArray = [];
+    historyArray.push(noteText);
+    let arrayStringfy = JSON.stringify(historyArray);
+    localStorage.setItem("historyNotes", arrayStringfy);
+  }
+}
+
+function getLS() {
+  return JSON.parse(localStorage.getItem("historyNotes"));
+}
+
+function showSavedNotes() {
+  let oldNotes = getLS();
+
+  oldNotes.forEach((eachNotes) => {
+    const div = document.createElement("div");
+    div.innerText = eachNotes;
+    div.classList.add("noteBox");
+
+    // --Craete remove button (X)
+    // Create <a> tag for remove button
+    const removeBtn = document.createElement("a");
+
+    // Set (X) to remove button
+    removeBtn.innerText = "✖";
+    // Set class for removeBtn
+    removeBtn.classList.add("removeBtn");
+
+    // Adding removeBtn to <div>
+    div.appendChild(removeBtn);
+    // Adding <div> to noteList
+    noteList.appendChild(div);
+  });
 }
