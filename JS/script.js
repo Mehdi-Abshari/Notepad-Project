49161f592c2f8dd1abc6dde2159fc8ee5e843efa
Mehdi-Abshari <absharimahdi5@gmail.com>
2023-01-06 01:56:10 +0330

    fixed local storage and add css style
    محل حافظه نوت ها فیکس شد و به دکمه پاک کردن استایل داده شد!

--- JS/script.js
+++ JS/script.js
@@ -8,6 +8,7 @@ const noteList = document.querySelector("#note-list");
 // submit new note
 form.addEventListener("submit", newNote);
 document.addEventListener("DOMContentLoaded", showSavedNotes);
+noteList.addEventListener("click", removeNote);
 
 // --Function
 function newNote(e) {
@@ -16,9 +17,65 @@ function newNote(e) {
   // Access to textarea value
   const noteText = document.querySelector("#note").value;
 
-  // Craete <div> tag
+  createUINote(noteText);
+
+  // Adding Data to localstorage
+  addToLs(noteText);
+}
+
+function removeNote(e) {
+  if( e.target.classList.contains('removeBtn') ){
+    e.target.parentElement.remove();
+
+    removeFromLS(e.target.parentElement.textContent);
+  };
+
+
+}
+
+function removeFromLS(noteText){
+  let cleanNoteText = noteText.substring(0,noteText.length - 1)
+
+  let oldNotes = getLS();
+  oldNotes.forEach((eachNote, noteIndex)=>{
+    if( eachNote == cleanNoteText){
+      oldNotes.splice(noteIndex,1);
+    }
+  })
+
+  localStorage.setItem('historyNotes',JSON.stringify(oldNotes))
+
+  getLS()
+}
+
+
+
+function addToLs(noteText) {
+  if (localStorage.getItem("historyNotes")) {
+    let arrayParsed = getLS();
+    arrayParsed.push(noteText);
+    let arrayStringfy = JSON.stringify(arrayParsed);
+    localStorage.setItem("historyNotes", arrayStringfy);
+  } else {
+    let historyArray = [];
+    historyArray.push(noteText);
+    let arrayStringfy = JSON.stringify(historyArray);
+    localStorage.setItem("historyNotes", arrayStringfy);
+  }
+}
+
+function getLS() {
+  return JSON.parse(localStorage.getItem("historyNotes"));
+}
+
+function showSavedNotes() {
+  let oldNotes = getLS();
+  oldNotes.forEach((eachNote) => { createUINote(eachNote) });
+}
+
+function createUINote(noteValue) {
   const div = document.createElement("div");
-  div.innerText = noteText;
+  div.innerText = noteValue;
   div.classList.add("noteBox");
 
   // --Craete remove button (X)
@@ -34,52 +91,4 @@ function newNote(e) {
   div.appendChild(removeBtn);
   // Adding <div> to noteList
   noteList.appendChild(div);
-
-  // Adding Data to localstorage
-  addToLs(noteText);
-}
-
-function addToLs(noteText) {
-  if (localStorage.getItem("historyNotes")) {
-    alert("داره!");
-    let arrayParsed = getLS();
-    arrayParsed.push(noteText);
-    let arrayStringfy = JSON.stringify(arrayParsed);
-    localStorage.setItem("historyNotes", arrayStringfy);
-  } else {
-    alert("ندارهههههه!");
-
-    let historyArray = [];
-    historyArray.push(noteText);
-    let arrayStringfy = JSON.stringify(historyArray);
-    localStorage.setItem("historyNotes", arrayStringfy);
-  }
-}
-
-function getLS() {
-  return JSON.parse(localStorage.getItem("historyNotes"));
-}
-
-function showSavedNotes() {
-  let oldNotes = getLS();
-
-  oldNotes.forEach((eachNotes) => {
-    const div = document.createElement("div");
-    div.innerText = eachNotes;
-    div.classList.add("noteBox");
-
-    // --Craete remove button (X)
-    // Create <a> tag for remove button
-    const removeBtn = document.createElement("a");
-
-    // Set (X) to remove button
-    removeBtn.innerText = "✖";
-    // Set class for removeBtn
-    removeBtn.classList.add("removeBtn");
-
-    // Adding removeBtn to <div>
-    div.appendChild(removeBtn);
-    // Adding <div> to noteList
-    noteList.appendChild(div);
-  });
 }
